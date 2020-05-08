6a40bbe3caf1f2f0aa65076fcefe75392bdd2203
Sanjith <t-saathl@microsoft.com>
2020-05-08 10:41:48 +0000

    Updated run_varuna for gpt2

--- files_for_pretraining/run_varuna.py
+++ files_for_pretraining/run_varuna.py
@@ -4,6 +4,7 @@ import sys
 import subprocess
 import signal
 import math
+import socket
 # import atexit
 
 local_rank_to_device = [0,1,2,3]
@@ -12,13 +13,17 @@ local_rank_to_device = [0,1,2,3]
 MAX_GPU_MEM = 16280000000
 processes = []
 
+manager_ip = "172.16.5.4"
+manager_port = 4200
+
 def calculate_config(args):
     # world size in terms of number of processes
     gpus_available = args.ngpus_per_server * args.nservers
     gpus_per_stage = (gpus_available // args.nstages) if args.gpus_per_stage == 0 else args.gpus_per_stage
-    args.gpus_per_stage = gpus_per_stage
-    dist_world_size = args.gpus_per_stage * args.nstages
-    assert dist_world_size <= gpus_available, "Too many gpus_per_stage - {}!".format(args.gpus_per_stage)
+    # args.gpus_per_stage = gpus_per_stage
+    print(gpus_per_stage, "per stage")
+    dist_world_size = gpus_per_stage * args.nstages
+    assert dist_world_size <= gpus_available, "Too many gpus_per_stage - {}!".format(gpus_per_stage)
     unused_gpus = (gpus_available - dist_world_size)
 
     # one whole server is unused
@@ -37,9 +42,10 @@ def calculate_config(args):
         ranks = ",".join([str(r) for r in stage_to_rank_map[stage]])
         stage_to_rank_map_str += (ranks + ";")
 
-    # batch size should be divisible by num of data parallel workers
-    per_gpu_batch_size = args.batch_size // args.gpus_per_stage
-    train_batch_size = per_gpu_batch_size * args.gpus_per_stage
+    # # batch size should be divisible by num of data parallel workers
+    per_gpu_batch_size = args.batch_size // gpus_per_stage
+    total_batch_size = per_gpu_batch_size * gpus_per_stage
+    # train_batch_size = args.batch_size
     
     # per_gpu_micro_batch_size = math.ceil(per_gpu_batch_size / (1.0 * args.chunks))
     # gradient_accumulation_steps = 1
@@ -57,17 +63,6 @@ def calculate_config(args):
     # # we want to keep the micro BS same
     # args.chunks = args.chunks * gradient_accumulation_steps
 
-    # hard coded microbatch sizes for the current configs
-    if args.nstages == 4:
-        # 4x4 and 4x5
-        args.chunk_size = 128
-    elif args.nstages == 8:
-        # 8x2
-        args.chunk_size = 200
-    else:
-        #2x9
-        args.chunk_size = 64
-
     first_rank_in_server = args.node_rank * args.ngpus_per_server
     if args.node_rank == args.nservers - 1:
         ranks_in_server = range(first_rank_in_server, first_rank_in_server + args.ngpus_per_server - unused_gpus)
@@ -81,7 +76,7 @@ def calculate_config(args):
     print("data depth:", gpus_per_stage)
     print("stage to rank map:", stage_to_rank_map_str)
 
-    return dist_world_size, stage_to_rank_map_str, ranks_in_server, train_batch_size
+    return dist_world_size, stage_to_rank_map_str, ranks_in_server, total_batch_size, gpus_per_stage
     
 
 
@@ -109,10 +104,10 @@ def parse_args():
     parser.add_argument("--batch_size", required=True, type=int,
                         help="Total effective batch size required")
 
-    parser.add_argument("--profile", required=True, type=str,
-                        help="CSV file containing memory profile of the model")
+    # parser.add_argument("--profile", required=True, type=str,
+    #                     help="CSV file containing memory profile of the model")
     
-    parser.add_argument("--chunk_size", type=int, default=-1,
+    parser.add_argument("--chunk_size", type=int, required=True,
                         help="Micro-batch size per mini-batch")
 
     # need a better way to pass this information ?
@@ -152,28 +147,27 @@ if __name__ == "__main__":
 
     args = parse_args()
 
-    max_micro_batch_size_per_gpu = -1
-    cutpoints_per_stage = args.total_num_stages // args.nstages
+    # max_micro_batch_size_per_gpu = -1
+    # cutpoints_per_stage = args.total_num_stages // args.nstages
+    # with open(args.profile, 'r') as f:
+    #     # skip line with column names
+    #     f.readline()
+    #     for line in f:
+    #         if line == "":
+    #             continue
+    #         batch_size, _f, _b, max_mem_usage, _i , _m, _acts_size, _ = line.split(",")
+    #         # profile is for single stage, so scale acc
+    #         max_mem_usage = int(max_mem_usage) * cutpoints_per_stage
+    #         batch_size = int(batch_size)
+    #         if max_mem_usage > MAX_GPU_MEM:
+    #             break
+    #         else:
+    #             max_micro_batch_size_per_gpu = batch_size
 
-    with open(args.profile, 'r') as f:
-        # skip line with column names
-        f.readline()
-        for line in f:
-            if line == "":
-                continue
-            batch_size, _f, _b, max_mem_usage, _i , _m, _acts_size, _ = line.split(",")
-            # profile is for single stage, so scale acc
-            max_mem_usage = int(max_mem_usage) * cutpoints_per_stage
-            batch_size = int(batch_size)
-            if max_mem_usage > MAX_GPU_MEM:
-                break
-            else:
-                max_micro_batch_size_per_gpu = batch_size
+    # print("max_micro_batch_size_per_gpu:",max_micro_batch_size_per_gpu)
 
-    print("max_micro_batch_size_per_gpu:",max_micro_batch_size_per_gpu)
-
-    if max_micro_batch_size_per_gpu <= 0:
-        raise ValueError("No micro-batch can fit for the model! Calculated max micro BS per gpu is " + str(max_micro_batch_size_per_gpu))
+    # if max_micro_batch_size_per_gpu <= 0:
+    #     raise ValueError("No micro-batch can fit for the model! Calculated max micro BS per gpu is " + str(max_micro_batch_size_per_gpu))
 
     # for max GPU util
     if args.chunk_size == -1:
@@ -190,7 +184,7 @@ if __name__ == "__main__":
 
     def handler(signum,_):
         global loop_pending
-        print('Signal handler called with signal', signum)
+        print('Signal handler called with signal', signum, flush=True)
         with open('ngpus','r') as f:
             ngpus_per_server = int(f.read())
         with open('nservers','r') as f:
@@ -210,7 +204,7 @@ if __name__ == "__main__":
         args.nstages = nstages
         for p in processes:
             p.send_signal(signal.SIGUSR1)
-        print("\n\n CONFIG CHANGED TO ",args.nstages, "x",args.gpus_per_stage,"!!\n\n\n")
+        print("\n\n CONFIG CHANGED TO ",args.nservers, "x",args.ngpus_per_server,"!!\n\n\n", flush=True)
 
     signal.signal(signal.SIGUSR1, handler)
 
@@ -234,7 +228,7 @@ if __name__ == "__main__":
         processes = []
         loop_pending = False
 
-        dist_world_size, stage_to_rank_map_str, ranks_in_server, train_batch_size = calculate_config(args)
+        dist_world_size, stage_to_rank_map_str, ranks_in_server, total_batch_size, gpus_per_stage = calculate_config(args)
         current_env["WORLD_SIZE"] = str(dist_world_size)
         print("World size is",dist_world_size)
         
@@ -256,18 +250,20 @@ if __name__ == "__main__":
 
             cmd.append(args.training_script)
 
+            per_process_batch_size = total_batch_size // gpus_per_stage
+
             cmd.append("--rank={}".format(str(rank)))
             cmd.append("--partitions={}".format(str(args.nstages)))
             cmd.append("--chunk_size={}".format(str(args.chunk_size)))
             cmd.append("--local_rank={}".format(str(local_rank)))
             cmd.append("--stage_to_rank_map={}".format(stage_to_rank_map_str))
-            cmd.append("--device={}".format(str(local_rank_to_device[local_rank])))
-            cmd.append("--train_batch_size={}".format(str(train_batch_size)))
-            if loop_count > 0:
-                with open("resume_step","r") as f:
-                    resume_step = int(f.read())
-                cmd.append("--resume_from_checkpoint")
-                cmd.append("--resume_step={}".format(resume_step))
+            # cmd.append("--device={}".format(str(local_rank_to_device[local_rank])))
+            cmd.append("--batch-size={}".format(str(per_process_batch_size)))
+            # if loop_count > 0:
+            #     with open("resume_step","r") as f:
+            #         resume_step = int(f.read())
+            #     cmd.append("--resume_from_checkpoint")
+            #     cmd.append("--resume_step={}".format(resume_step))
 
             cmd.extend(args.training_script_args)
             print(" ".join(cmd))
@@ -283,17 +279,34 @@ if __name__ == "__main__":
             f.write("notdone")
 
         # wait for all processes
-        for process in processes:
-            process.wait()
-            print("Process done with return code", process.returncode)
-            if process.returncode != 0:
-                for p in processes:
-                    p.kill()
-                raise subprocess.CalledProcessError(returncode=process.returncode,
-                                                    cmd=cmd)
+        try:
+            for process in processes:
+                process.wait()
+                print("Process done with return code", process.returncode)
+                if process.returncode != 0:
+                    for p in processes:
+                        p.kill()
+                    # raise subprocess.CalledProcessError(returncode=process.returncode,
+                                                        # cmd=cmd)
+        except Exception as e:
+            print("run_varuna subprocesses quit with error:", e)
 
-        with open("prev_job_done","w")as f:
+        with open("prev_job_done","w") as f:
             f.write("done")
+
+        last_iter = -1
+        if os.path.exists("/home/varuna/local_ckpt_tracker.txt"):
+            with open("/home/varuna/local_ckpt_tracker.txt","r") as f:
+                last_iter = int(f.read())
+
+        print("done and trying to send here", flush=True)
+        
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            message = "checkpoint done {}".format(last_iter)
+            sock.connect((manager_ip, manager_port))
+            sock.sendall(bytes(message, 'ascii'))
+
+        print("and sent!")
 
         loop_count += 1
 
