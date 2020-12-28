b10656d9d7b436bf131d16c2baf94f3d825432f9
Ubuntu <varuna@manager.4xowgm2dkwievm0fuzerbwqkph.jx.internal.cloudapp.net>
2020-12-28 13:52:13 +0000

    remove unnecessary

--- Megatron-LM/examples/pretrain_gpt2.sh
+++ Megatron-LM/examples/pretrain_gpt2.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
-export PATH="/home/varuna/anaconda3/bin:$PATH"
+#export PATH="/home/varuna/anaconda3/bin:$PATH"
+#which conda
 
 RANK=0
 WORLD_SIZE=2
@@ -13,13 +14,15 @@ echo $NNODES $NODE_RANK $MASTER_ADDR $ckpt
 date
 ifconfig eth0 | grep inet
 
-DATA_PATH=/home/varuna/gpt2-blob/openwebtext_full/openwebtext_text_document
-CHECKPOINT_PATH=/home/varuna/gpt2-blob/varuna_20b_8k_1.25e-3
+DATA_PATH=/home/varuna/gpt2-blob/turing/megatron
+CHECKPOINT_PATH=/home/varuna/gpt2-blob/mega_2_5b_4.8e-3_clipgrads
 
-NCCL_SOCKET_IFNAME=eth0 NCCL_SOCKET_NTHREADS=4 NCCL_NSOCKS_PERTHREAD=4 python3 run_varuna.py --nstages 49 --batch_size 8192 --chunk_size 2 --total_num_stages 98 --ngpus_per_server 4 --nservers $NNODES --node_rank $NODE_RANK --master_addr $MASTER_ADDR  pretrain_gpt2.py \
-       --num-layers 96 \
-       --hidden-size 4200 \
-       --num-attention-heads 42 \
+NCCL_DEBUG=INFO NCCL_SOCKET_IFNAME=eth0 NCCL_SOCKET_NTHREADS=4 NCCL_NSOCKS_PERTHREAD=4 \
+python3 run_varuna.py --nstages 9 --batch_size 8192 --chunk_size 4 --total_num_stages 54 \
+       --ngpus_per_server 4 --nservers $NNODES --node_rank $NODE_RANK --master_addr $MASTER_ADDR pretrain_gpt2.py \
+       --num-layers 54 \
+       --hidden-size 1920 \
+       --num-attention-heads 20 \
        --seq-length 1024 \
        --max-position-embeddings 1024 \
        --train-iters 18750 \
@@ -32,20 +35,20 @@ NCCL_SOCKET_IFNAME=eth0 NCCL_SOCKET_NTHREADS=4 NCCL_NSOCKS_PERTHREAD=4 python3 r
        --data-impl mmap \
        --split 949,50,1 \
        --distributed-backend gloo \
-       --lr 0.00125 \
-       --min-lr 1.6e-4 \
+       --lr 0.0048 \
+       --min-lr 1e-5 \
        --lr-decay-style cosine \
        --weight-decay 1e-2 \
        --clip-grad 1.0 \
        --warmup .05 \
        --log-interval 1 \
        --save-interval 15 \
-       --max-num-ckpts 3 \
-       --min-ckpt-iter-to-remove 1410 \
+       --max-num-ckpts 10 \
+       --min-ckpt-iter-to-remove 3100 \
        --load-iteration $ckpt \
        --eval-interval 100 \
        --eval-iters 10 \
-       --loss-file varuna_20b_8k \
+       --loss-file varuna_2_5b_8192_lr_0.0048_clipgrads \
        --fp16 --varuna
 # num params = 20,753,384,400
 set +x
